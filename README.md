ac564c3ca28f63f74697cba447d8de9443984c58
Richie S <richie.siow@gmail.com>
2021-03-01 20:57:42 +1100

    enabled API hider

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#Simple Weather App.
+#Simple Weather App utilising https://openweathermap.org
 
 #Input the city of your choice in the text box and it will fetch you the current temperature, what it feels like and weather conditions.
 
-#Check out the app @ https://rsiow.github.io/Weather-app/
+#Check out a working version of the app @ https://rsiow.github.io/Weather-app/
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Set API Key
-const apiKey = 'Set API Key here';
+require("dotenv").config();
+const appId = process.env.API_KEY;
+
 
 const cityInput = document.querySelector('#city-input');
 const submitInput = document.querySelector('#weather-button');
 const weatherOutput = document.querySelector('#weather-output');
+
+const getDataForCity = cityInput => fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${appId}&units=metric`)
+    .then(response => response.json());
 
 // Card template to show weather info
 const createCardHtml = (name, temp, feelsLike, description) => {
@@ -28,16 +32,29 @@ const createCardHtml = (name, temp, feelsLike, description) => {
 };
 
 // On click of submit button, fetch data to input in weather card template
-submitInput.addEventListener('click', async () => {
-    const city = cityInput.value;
-    const getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-    const data = await getData.json();
-    const name = data.name;
-    const temp = data.main.temp;
-    const feelsLike = data.main.feels_like;
-    const description = data.weather[0].description;
+submitInput.addEventListener('click', () => {
+    let cityValue = cityInput.value
+    getDataForCity(cityValue).then(data => {
 
-    const cardHtml = createCardHtml(name, temp, feelsLike, description);
-    // Place data-filled template into main HTML for display
-    weatherOutput.innerHTML = cardHtml;
+        const name = data.name;
+        const temp = data.main.temp;
+        const feelsLike = data.main.feels_like;
+        const description = data.weather[0].description;
+        const cardHtml = createCardHtml(name, temp, feelsLike, description);
+        weatherOutput.innerHTML = cardHtml
+    })
 });
+
+// submitInput.addEventListener('click', async () => {
+//     const city = cityInput.value;
+//     const getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}&units=metric`);
+//     const data = await getData.json();
+//     const name = data.name;
+//     const temp = data.main.temp;
+//     const feelsLike = data.main.feels_like;
+//     const description = data.weather[0].description;
+
+//     const cardHtml = createCardHtml(name, temp, feelsLike, description);
+//     // Place data-filled template into main HTML for display
+//     weatherOutput.innerHTML = cardHtml;
+// });
